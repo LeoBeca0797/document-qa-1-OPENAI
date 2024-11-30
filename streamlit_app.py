dca7c80f45c6db7e54fcc4012092c7dc57020baa
Leonardo Becarelli <leobecarelli97@gmail.com>
2024-11-30 16:55:05 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,8 +63,7 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
-st.markdown('<div class="main-header">Benvenuta sul Primula</div>', unsafe_allow_html=True)
-st.markdown('<div class="sub-header">Il tuo aiuto nell\'interpretazione e la raccolta delle competenze socio-emotive!</div>', unsafe_allow_html=True)
+st.image("./Primula.png", use_column_width=True)
 
 # Sidebar with Styled Header
 st.sidebar.markdown("### 📝 Elenco Studenti")
